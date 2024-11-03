a00be40bace51740adf3ba7dd68d3b23e001bd59
Keonity <56704285+Keonity@users.noreply.github.com>
2024-11-03 02:31:52 -0800

    Added boilerplate HTML and JS, removed template query function.

--- src/index.js
+++ src/index.js
@@ -11,34 +11,6 @@ const loadAnimation = document.querySelector(`#loadAnimationOff`);
 
 const queryLocation = `Las Vegas`;
 
-async function getWeather(data = `Las Vegas`) {
-    try {
-        loadAnimation.setAttribute(`id`, `loadAnimation`);
-        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${data}?unitGroup=metric&key=5MGVRN4YAV8UJY9MGZB77KHLE&contentType=json`);
-        if (!response.ok) {
-            throw new Error(response.status)
-        }
-        let weatherData = await response.json();
-        loadAnimation.setAttribute(`id`, `loadAnimationOff`)
-        console.log(weatherData.currentConditions.temp);
-        resultHeader.innerHTML = `The temperature in ${weatherData.resolvedAddress} is ${weatherData.currentConditions.temp}` + `&#8451`;
-        description.innerHTML = `${weatherData.description}`
-    }
-    catch (error) {
-        if (error.message === `400`) {
-            loadAnimation.setAttribute(`id`, `loadAnimationOff`);
-            resultHeader.innerHTML = `Error ${error.message}: Invalid search parameters, please try again with a different location.`;
-        }
-        else {
-            loadAnimation.setAttribute(`id`, `loadAnimationOff`);
-            resultHeader.innerHTML = `Error ${error.message}`;
-        }
-        description.innerHTML = ``;
-    }
-}
-
 submit.addEventListener("click", () => {
-    getWeather(searchbar.value);
+    console.log(`Hello there! :)`)
 });
-
-getWeather();
