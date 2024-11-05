221e8324502e5b9b6e8f78e2a778b9a8cbfd36a3
Keonity <56704285+Keonity@users.noreply.github.com>
2024-11-04 21:05:54 -0800

    Added find possible moves function.

--- src/index.js
+++ src/index.js
@@ -12,5 +12,49 @@ const loadAnimation = document.querySelector(`#loadAnimationOff`);
 const queryLocation = `Las Vegas`;
 
 submit.addEventListener("click", () => {
-    console.log(`Hello there! :)`)
+    console.log(`Hello there! :)`);
 });
+
+function findPossibleMoves(x, y) {
+    console.log(`Finding possible moves for ${x}, ${y}`);
+
+    if (((x - 1) >= 0) && ((y + 2) <= 7)) {
+        console.log(`${x - 1}, ${y + 2}`);
+    }
+
+    if (((x - 1) >= 0) && ((y - 2) >= 0)) {
+        console.log(`${x - 1}, ${y - 2}`);
+    }
+
+    if (((x + 1) >= 0) && ((y + 2) <= 7)) {
+        console.log(`${x + 1}, ${y + 2}`);
+    }
+
+    if (((x + 1) >= 0) && ((y - 2) >= 0)) {
+        console.log(`${x + 1}, ${y - 2}`);
+    }
+
+    if (((x - 2) >= 0) && ((y + 1) <= 7)) {
+        console.log(`${x - 2}, ${y + 1}`);
+    }
+
+    if (((x - 2) >= 0) && ((y - 1) >= 0)) {
+        console.log(`${x - 2}, ${y - 1}`);
+    }
+
+    if (((x + 2) <= 7) && ((y + 1) <= 7)) {
+        console.log(`${x + 2}, ${y + 1}`);
+    }
+
+    if (((x + 2) <= 7) && ((y - 1) >= 0)) {
+        console.log(`${x + 2}, ${y - 1}`);
+    }
+
+    console.log(`- - - - -`);
+}
+
+findPossibleMoves(3, 3);
+
+findPossibleMoves(0, 0);
+
+findPossibleMoves(6, 3);
