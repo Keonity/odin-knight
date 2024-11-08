b5641f60c17b03a4c428b9a1f2611f6d77e31574
Keonity <56704285+Keonity@users.noreply.github.com>
2024-11-07 20:03:49 -0800

    Added recursive find possible moves function.

--- src/index.js
+++ src/index.js
@@ -15,11 +15,85 @@ submit.addEventListener("click", () => {
     console.log(`Hello there! :)`);
 });
 
+function findPossibleMovesRecursive(x, y, hist) {
+    console.log(`Finding possible moves recursively for ${x}, ${y}`);
+    // Add hist push
+    hist.push(`${x}, ${y}`);
+
+    if (((x - 1) >= 0) && ((y + 2) <= 7) && !hist.includes(`${x - 1}, ${y + 2}`)) {
+        const newArray = Array.from(hist);
+        // newArray.push(`${x - 1}, ${y + 2}`);
+        findPossibleMovesRecursive(x - 1, y + 2, newArray);
+        console.log(`${x - 1}, ${y + 2}`);
+        console.log(newArray);
+    }
+
+    if (((x - 1) >= 0) && ((y - 2) >= 0) && !hist.includes(`${x - 1}, ${y - 2}`)) {
+        const newArray = Array.from(hist);
+        newArray.push(`${x - 1}, ${y - 2}`);
+        // findPossibleMovesRecursive(x - 1, y - 2, newArray);
+        console.log(`${x - 1}, ${y - 2}`);
+        console.log(newArray);
+    }
+
+    if (((x + 1) <= 7) && ((y + 2) <= 7) && !hist.includes(`${x + 1}, ${y + 2}`)) {
+        const newArray = Array.from(hist);
+        newArray.push(`${x + 1}, ${y + 2}`);
+        // findPossibleMovesRecursive(x + 1, y + 2, newArray);
+        console.log(`${x + 1}, ${y + 2}`);
+        console.log(newArray);
+    }
+
+    if (((x + 1) <= 7) && ((y - 2) >= 0) && !hist.includes(`${x + 1}, ${y - 2}`)) {
+        const newArray = Array.from(hist);
+        newArray.push(`${x + 1}, ${y - 2}`);
+        // findPossibleMovesRecursive(x + 1, y - 2, newArray);
+        console.log(`${x + 1}, ${y - 2}`);
+        console.log(newArray);
+    }
+
+    if (((x - 2) >= 0) && ((y + 1) <= 7) && !hist.includes(`${x - 2}, ${y + 1}`)) {
+        const newArray = Array.from(hist);
+        newArray.push(`${x - 2}, ${y + 1}`);
+        // findPossibleMovesRecursive(x - 2, y + 1, newArray);
+        console.log(`${x - 2}, ${y + 1}`);
+        console.log(newArray);
+    }
+
+    if (((x - 2) >= 0) && ((y - 1) >= 0) && !hist.includes(`${x - 2}, ${y - 1}`)) {
+        const newArray = Array.from(hist);
+        newArray.push(`${x - 2}, ${y - 1}`);
+        // findPossibleMovesRecursive(x - 2, y - 1, newArray);
+        console.log(`${x - 2}, ${y - 1}`);
+        console.log(newArray);
+    }
+
+    if (((x + 2) <= 7) && ((y + 1) <= 7) && !hist.includes(`${x + 2}, ${y + 1}`)) {
+        const newArray = Array.from(hist);
+        newArray.push(`${x + 2}, ${y + 1}`);
+        // findPossibleMovesRecursive(x + 2, y + 1, newArray);
+        console.log(`${x + 2}, ${y + 1}`);
+        console.log(newArray);
+    }
+
+    if (((x + 2) <= 7) && ((y - 1) >= 0) && !hist.includes(`${x + 2}, ${y - 1}`)) {
+        const newArray = Array.from(hist);
+        newArray.push(`${x + 2}, ${y - 1}`);
+        // findPossibleMovesRecursive(x + 2, y - 1, newArray);
+        console.log(`${x + 2}, ${y - 1}`);
+        console.log(newArray);
+    }
+
+    // console.log(hist);
+    console.log(`- - - - -`);
+}
+
 function findPossibleMoves(x, y) {
     console.log(`Finding possible moves for ${x}, ${y}`);
 
     if (((x - 1) >= 0) && ((y + 2) <= 7)) {
         console.log(`${x - 1}, ${y + 2}`);
+        findPossibleMovesRecursive(x, y, []);
     }
 
     if (((x - 1) >= 0) && ((y - 2) >= 0)) {
@@ -53,8 +127,8 @@ function findPossibleMoves(x, y) {
     console.log(`- - - - -`);
 }
 
-findPossibleMoves(3, 3);
+// findPossibleMoves(3, 3);
 
-findPossibleMoves(0, 0);
+// findPossibleMoves(0, 0);
 
 findPossibleMoves(6, 3);
